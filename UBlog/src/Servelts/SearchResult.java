e5fdbfb9e5a6d0300f47d1e67667e6d8eb57837c
defaultcoder <defaultcoder@mail.ru>
2013-06-12 07:57:00 -0700

    dwada

---
+++ UBlog/src/Servelts/SearchResult.java
@@ -0,0 +1,67 @@
+package Servelts;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.jdbc.Connection;
+
+import Controllers.BlogController;
+import Controllers.DBConnector;
+import DB.Condition;
+import DB.DB;
+import DB.ResultList;
+import Models.Blog;
+
+/**
+ * Servlet implementation class SearchResult
+ */
+@WebServlet("/Search")
+public class SearchResult extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SearchResult() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String toSearch = request.getParameter("searchterm");
+		String curCat = request.getParameter("category");
+		ArrayList<Blog> arr = new ArrayList<Blog>();
+		DB db = new DB(((DBConnector)getServletContext().getAttribute("DBC")).getConnection(), "ublog");
+		Condition c = new Condition(true); 
+		c.add("tag", toSearch);
+		db.where(c);
+		ResultList rl = db.get("tag_article");
+		BlogController bc = new BlogController((Connection) ((DBConnector)getServletContext().getAttribute("DBC")).getConnection());
+		for(int i=0;i<rl.size();i++){
+			Blog b = bc.getBlogById(rl.get(i).get("Article_Id"));
+			if(curCat.equals("all") || b.getCategory().equals(curCat))
+				arr.add(b);
+		}
+		request.setAttribute("blogs", arr);
+		RequestDispatcher rd = request.getRequestDispatcher("search_result.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
