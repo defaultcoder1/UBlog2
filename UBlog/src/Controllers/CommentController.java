65dd9cf61e72d7676f648e0d6d7466d8feb025b3
defaultcoder0 <flower.cheza@gmail.com>
2013-06-13 07:36:24 +0400

    add an delete comment methods

--- UBlog/src/Controllers/CommentController.java
+++ UBlog/src/Controllers/CommentController.java
@@ -2,7 +2,9 @@ package Controllers;
 
 import java.sql.Connection;
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import DB.CString;
 import DB.Condition;
 import DB.DB;
 import DB.ResultList;
@@ -18,18 +20,39 @@ public class CommentController {
 	
 	public Comment getCommentById(String commentId){
 		DB db = new DB(this.con, "ublog");
-		Condition con = new Condition(true);
-		con.add("Comment_ID",commentId);
-		db.where(con);
+		Condition cond = new Condition(true);
+		cond.add("Comment_ID",commentId);
+		db.where(cond);
 		ResultList crl = db.get("comment");
 		db = new DB(this.con, "ublog");
-		con = new Condition(true);
-		con.add("User_ID", crl.first().get("User_ID"));
-		db.where(con);
+		cond = new Condition(true);
+		cond.add("User_ID", crl.first().get("User_ID"));
+		db.where(cond);
 		ResultList url = db.get("user");
 		LikeController lc = new LikeController(this.con);
 		Comment c = new Comment(commentId, url.first().get("User_ID"), crl.first().get("Content"), crl.first().get("Article_ID"),crl.first().get("Date"),
 		url.first().get("FName"), url.first().get("LName"),url.first().get("Image"), lc.getCommentLikes(commentId));
 		return c;
+	}
+
+
+	public boolean deleteComment(String commentId){
+		DB db = new DB (this.con, "ublog");
+		Condition cond = new Condition(true);
+		cond.add("Comment_ID", commentId);
+		db.where(cond);
+		int result = db.delete("comment");
+		return result>0; 
+	}
+	
+	public String addComment(String userId, String articleId, String content){
+		DB db = new DB (this.con, "ublog");
+		CString commentId = new CString ("");
+		HashMap <String, String> m = new HashMap<String, String>();
+		m.put("User_ID", userId);
+		m.put("Article_ID", articleId);
+		m.put("Content", content);
+		db.insert("comment", m, commentId);
+		return commentId.toString();
 	}
 }
